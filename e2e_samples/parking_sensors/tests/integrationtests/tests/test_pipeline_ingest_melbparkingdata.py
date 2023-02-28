c584208e316de13f3bbb512aa5c25b9dc8b7d335
Isabel Cabezas <isacabe@microsoft.com>
2023-02-28 17:59:44 +0100

    download ubuntu packages

--- e2e_samples/parking_sensors/tests/integrationtests/tests/test_pipeline_ingest_melbparkingdata.py
+++ e2e_samples/parking_sensors/tests/integrationtests/tests/test_pipeline_ingest_melbparkingdata.py
@@ -6,11 +6,8 @@ PIPELINE_NAME = "P_Ingest_MelbParkingData"
 
 
 def test_pipeline_succeeded(adf_pipeline_run, sql_connection):
-    """Test that pipeline has data in SQL"""
-    print(load_id)
-    print({load_id})
-    
-    
+    """Test that pipeline has data in SQL"""    
+
     this_run = adf_pipeline_run(PIPELINE_NAME, run_inputs={})
     print(this_run.run_id)
 
@@ -20,6 +17,8 @@ def test_pipeline_succeeded(adf_pipeline_run, sql_connection):
         "SELECT COUNT(*) AS COUNT FROM dbo.fact_parking WHERE load_id='{load_id}'"
         .format(load_id=str(this_run.run_id)))
     row = cursor.fetchone()
+
+    print(this_run.status)
     # assert this_run.status == "Succeeded"
     assert "Succeeded"== "Succeeded"
     # assert row is not None
